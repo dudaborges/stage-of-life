03ea82a0009ea9bc3457f2d9ee81955554dd577c
dudaborges <duda.pborges92@gmail.com>
2022-08-29 17:51:45 -0300

    update

---
+++ script.js
@@ -0,0 +1,24 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    //pega ano com 4 dígitos
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || fano.value > ano){
+        alert('[ERRO] Verifique os dados e tente novamente!')
+    } else{
+        var fsex = document.getElementsByName('radsex') //se fosse pelo id teria que fazer duas variáveis
+        var idade = ano - fano.value
+        var genero = ''
+        
+        //Se o formulário sexo for da posição zero selecionado será masculino
+        if(fsex[0].checked){
+            genero = 'Homem'
+        } else {
+            genero = 'Feminino'
+        }
+
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    }
+}
